2c4c121f70d75abc740fde8fa823fbc682bd03b8
Palivec17 <palivec17@seznam.cz>
2025-10-04 09:45:52 +0200

    Vytvořena funkce vstupních argumentů

--- main.py
+++ main.py
@@ -5,3 +5,19 @@ author: Petr Polívka
 email: palivec17@seznam.cz
 """
 
+
+import sys
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+
+# Zadané argumenty
+def validate_arguments(args):
+    if len(args) != 3:
+        print("❌ Chyba: Musíte zadat přesně dva argumenty – URL a název výstupního souboru.")
+        print("Použití: python main.py <platná_URL> <výstupní_soubor.csv>")
+        sys.exit(1)
+    if not args[1].startswith("https://www.volby.cz/pls/ps2017nss/"):
+        print("❌ Chyba: První argument musí být platná URL začínající na 'https://www.volby.cz/pls/ps2017nss/'.")
+        sys.exit(1)
