e9805e3b4aa3d74c23b796ff2c9584ff9f68977d
Palivec17 <palivec17@seznam.cz>
2025-10-04 09:48:45 +0200

    Vytvořena funkce na odkazy na obce

--- main.py
+++ main.py
@@ -21,3 +21,17 @@ def validate_arguments(args):
     if not args[1].startswith("https://www.volby.cz/pls/ps2017nss/"):
         print("❌ Chyba: První argument musí být platná URL začínající na 'https://www.volby.cz/pls/ps2017nss/'.")
         sys.exit(1)
+
+# Odkazy na obce
+def get_obec_links(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    links = []
+    for a in soup.find_all("a"):
+        href = a.get("href")
+        if href and "xobec" in href:
+            full_url = "https://www.volby.cz/pls/ps2017nss/" + href
+            links.append(full_url)
+    return links
+
+
